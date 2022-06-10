2229318573883b34b8c03a55bb6610eeeaec31f8
Umang <umang.srivastava.ckc@gmail.com>
2022-06-11 05:12:14 +0530

    - converted single & doubly linkedlist to use generic - converted Sort to use generics.

--- errors/errors.go
+++ errors/errors.go
@@ -4,4 +4,6 @@ import "errors"
 
 var (
 	UninitializedError = errors.New("the data-structure was uninitialized")
+	IndexOutOfBound    = errors.New("index out of bound")
+	NotFound           = errors.New("value not found")
 )
