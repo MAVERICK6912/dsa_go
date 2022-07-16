12bc0b57eabe82d61c2f66e34429d40d408cadf4
Umang <umang.srivastava.ckc@gmail.com>
2022-07-16 19:02:37 +0530

    - fixed comments

--- sort/quicksort.go
+++ sort/quicksort.go
@@ -27,22 +27,38 @@ func QuickSort[T any](it []T, sortDir SortDirection, cmp func(T, T) int) ([]T, e
 		- it[0]:=it[3]->it[0] = 1
 		- it[high]=t-> it[3]= 4
 		- return pRight+1 -> -1+1 -> 0
-		- it at the end of this function call will be: [1,3,2,4]
+		- `it` at the end of this function call will be: [1,3,2,4]
 		- partitionAndQuickSort([1,3,2,4],0,-1,ascending,cmp)-> this call will not do anything as low<high condition will be false.
 		- partitionAndQuickSort([1,3,2,4],1,3,ascending,cmp):
 			- if low<high: (1<3)-> true:
 				- partitionAscending([4,3,2,1],1,3,cmp):
 					- pivot, pRight :=(it[3])->4,(low-1)->0
 					- for i:=1;i<3;i++:
-						- if it[1]<pivot(4): (4<3)-> false
-						- if it[2]<pivot(4): (4<2)-> false
-					- t:=it[0+1]-> t:= it[1]-> t:=3
-					- it[1]:=it[3]->it[1] = 4
-					- it[high]=t-> it[3]= 3
-					- return pRight+1 -> 0+1 -> 1
-					- it at the end of this function call will be: [1,4,2,3]
-		- partitionAndQuickSort([1,3,2,4],0,0,ascending,cmp)-> this call will not do anything as low<high condition will be false.
-		- partitionAndQuickSort([1,3,2,4],2,3,ascending,cmp):
+						- if it[1]<pivot(4): (3<4)-> true:
+							- swap(it[1],it[1])->[1,3,2,4]
+							- pRight++=>1
+						- if it[2]<pivot(4): (2<4)-> true:
+							- swap(it[2],it[2])->[1,3,2,4]
+							- pRight++=>2
+					- t:=it[2+1]-> t:= it[3]-> t:=4
+					- it[3]:=it[3]->it[3] = 4
+					- it[high]=t-> it[3]= 4
+					- return pRight+1 -> 2+1 -> 3
+					- `it` at the end of this function call will be: [1,3,2,4]
+		- partitionAndQuickSort([1,3,2,4],4,3,ascending,cmp)-> this call will not do anything as low<high condition will be false.
+		- partitionAndQuickSort([1,3,2,4],1,2,ascending,cmp):
+		- if low<high: (1<2)->true:
+			- partitionAcending([1,3,2,4],1,2):
+				- pivot,pRight:= (it[2])->2,0
+				- for i:=1;1<2;i++:
+					- if(3<2):false
+				- swap(it[1],it[2])
+				- Array after swap: [1,2,3,4]
+				- return pRight+1-> 0+1-> 1
+				- `it` at the end of this function call will be: [1,2,3,4]
+		- partitionAndQuickSort([1,3,2,4],1,0,ascending,cmp)-> this call will not do anything as low<high condition will be false.
+		- partitionAndQuickSort([1,3,2,4],2,2,ascending,cmp)-> this call will not do anything as low<high condition will be false.
+		- Hence we will return: [1,2,3,4]
 */
 // partitionAnQuickSort partitions and sorts the slice recursively.
 func partitionAndQuickSort[T any](it []T, low, high int, sortDir SortDirection, cmp func(T, T) int) []T {
@@ -60,12 +76,6 @@ func partitionAndQuickSort[T any](it []T, low, high int, sortDir SortDirection, 
 	return it
 }
 
-/*
-	Approach:
-		- We choose a pivot(in our case its the last element of the array)
-		- Now, we put the pivot at the correct position.
-		- After that we put all elements smaller than pivot to the left of pivot and elements larger to the right of pivot.
-*/
 // partitionAscending partitions and sorts the slice in ascending order
 func partitionAscending[T any](it []T, low, high int, cmp func(T, T) int) int {
 	pivot, pRight := it[high], low-1
